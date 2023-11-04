a336a02fadf53b9d0d215dcce0bc1f2988c2845c
AnaAlonso679 <136395144+AnaAlonso679@users.noreply.github.com>
2023-11-04 12:42:28 -0600

    agregue clases

---
+++ clases.py
@@ -0,0 +1,10 @@
+class calculadora:
+    def sumar(numero,numero2):
+        return numero+numero2
+    def restar(numero,numero2):
+        return numero-numero2    
+    def multiplicar(numero,numero2):
+        return numero*numero2
+    def dividir(numero,numero2):
+        return numero/numero2
+    
