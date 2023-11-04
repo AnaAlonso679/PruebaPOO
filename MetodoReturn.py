581dffdcfe76d4131f42b0154dd490b2ac81e2cf
AnaAlonso679 <136395144+AnaAlonso679@users.noreply.github.com>
2023-11-04 11:53:53 -0600

    agregue metodosReturn

--- MetodoReturn.py
+++ MetodoReturn.py
@@ -2,6 +2,10 @@ def mostrar():
     return "Hola como"
 def sumar():
     return 10+5 
+def restar(numero,numero2):
+    return(numero+numero2)
+#metodo 3
+print(restar(2,2))
 #metodo 1
 print(sumar())
 #metodo 2
