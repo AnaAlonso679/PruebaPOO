eb17a2d95bf48622d671e59fdced968251c8587d
AnaAlonso679 <136395144+AnaAlonso679@users.noreply.github.com>
2024-01-06 12:49:19 -0600

    funciones

---
+++ Funciones.py
@@ -0,0 +1,28 @@
+def checada_entrada():
+
+    print("Ingresa el nombre de la persona")
+    NombrePersona=input()
+    print("Ingresa el modelo de carro")
+    carro=input()
+    print("Ingresa la matricula del carro")
+    Matricula=input()
+    print("Ingresa la fecha en formato dd-mm-aaaa")
+    Fecha=input()
+    print("Ingresa la hora en formato de 24 hrs")
+    Hora=input()
+    lista=[NombrePersona,carro,Matricula,Fecha,Hora]
+    return lista
+
+def checada_salida():
+    print("Ingresa el nombre de la persona")
+    NombrePersona=input()
+    print("Ingresa el modelo de carro")
+    carro=input()
+    print("Ingresa la matricula del carro")
+    Matricula=input()
+    print("Ingresa la fecha en formato dd-mm-aaaa")
+    Fecha=input()
+    print("Ingresa la hora en formato de 24 hrs")
+    Hora=input()
+    lista=[NombrePersona,carro,Matricula,Fecha,Hora]
+    return lista
---
+++ SistemaChecador.py
@@ -0,0 +1,7 @@
+from Funciones import checada_entrada, checada_salida
+
+datos_entrada=checada_entrada()
+print(datos_entrada)
+
+for datos in datos_entrada:
+    print(datos)
